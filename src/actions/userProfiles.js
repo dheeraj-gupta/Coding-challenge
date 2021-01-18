1d8b48c12bb8005416e7752378088877c3e9362c
dheeraj <dheeraj.guptait@gmail.com>
2021-01-18 12:08:31 +1100

    fixed small bug

--- src/actions/userProfiles.js
+++ src/actions/userProfiles.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
-import { USER_LOADED, AUTH_ERROR} from './types';
-// Load User 
-export const loadUser = () => async dispatch => {
-    try {
-       const res = await axios.get('http://demo0718548.mockable.io/cryptos');
-       dispatch({
-           type: USER_LOADED,
-           payload: res.data
-       })
-    } catch (err) {
-        dispatch({
-            type: AUTH_ERROR
-        })
-    }
-}
+import data from "../data/crypto.json";
+
+import { USER_LOADED, AUTH_ERROR } from "./types";
+// Load User
+export const loadUser = () => (dispatch) => {
+  try {
+    const res = data;
+    dispatch({
+      type: USER_LOADED,
+      payload: res,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
